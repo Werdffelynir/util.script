bcf7dd034fd9dcc52dbb3da13a732886f319428d
werdffelynir <werdffelynir@gmail.com>
2016-02-25 15:09:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Module util.js
+
+Its static common helpers methods
+
+## Methods
+
+```
+Util.objClone (obj)                         // Clone object
+Util.objLen (obj)                           // Count object length
+Util.objMerge (objectBase, src)             // Merge an object `src` into the object `objectBase`
+Util.objMergeNotExists (objectBase, src)    // Merge objects if `objectBase` key not exists
+Util.objMergeOnlyExists (objectBase, src)   // Merge objects if `objectBase` key is exists
+Util.arrDiff (arr1, arr2)                   // Computes the difference of arrays
+Util.isStr (param)                          // Check on typeof is string a param
+Util.isArr (param)                          // Check on typeof is array a param
+Util.isObj (param)                          // Check on typeof is object a param
+Util.isNum (param)                          // Determine param is a number or a numeric string
+Util.defined (param)                        // Determine param to undefined type
+Util.isEmpty (param)                        // Determine whether a variable is empty
+Util.objToJson (data)                       // Javascript object to JSON data
+Util.jsonToObj (data)                       // JSON data to Javascript object
+Util.cleanArr (src)                         // Cleans the array of empty elements
+Util.typeOf (data)                          // Return type of data
+Util.formData (form, asObject)              // Convert HTML form to encode URI string
+Util.toNode (data)                          // HTML string convert to DOM Elements Object
+Util.uniqueArr (arr)                        // Removes duplicate values from an array
+Util.fileGetContents (url)                  // Reads entire file into a string
+Util.getPosition (elem)                     // Calculates the position and size of elements.
+Util.getMouseElement (element, event)       // Returns the coordinates of the mouse on any element
+Util.getMouseCanvas (canvas, event)         // Returns the coordinates of the mouse on the canvas element
+Util.createStyle (selector, property)       // Creator of styles, return style-element or style-text.
+Util.createNode (tag, attrs, inner)         // Create new NodeElement
+Util.rand (min, max)                        // Returns a random integer between min, max
+Util.randColor ()                           // Returns random string color, HEX format
+Util.degreesToRadians (deg)                 // Converts degrees to radians
+Util.radiansToDegrees (rad)                 // Converts radians to degrees
+Util.distanceBetween (point1, point2)       // The calculation of the distance between points
+```
---
+++ src/util.js
@@ -0,0 +1,448 @@
+/**
+ * Module util.js
+ * Its static common helpers methods
+ */
+
+(function (window) {
+
+    if (!Array.isArray) {
+        Array.isArray = function (arg) {
+            return Object.prototype.toString.call(arg) === '[object Array]';
+        };
+    }
+
+    var o = {};
+
+    /**
+     * Clone object
+     * @param obj
+     * @returns {*}
+     */
+    o.objClone = function (obj) {
+        if (obj === null || typeof obj !== 'object') return obj;
+        var temp = obj.constructor();
+        for (var key in obj)
+            temp[key] = o.objClone(obj[key]);
+        return temp;
+    };
+
+    /**
+     * Count object length
+     * @param obj
+     * @returns {number}
+     */
+    o.objLen = function (obj) {
+        var it = 0;
+        for (var k in obj) it++;
+        return it;
+    };
+
+    /**
+     * Merge an object `src` into the object `objectBase`
+     * @param obj       main object of merge
+     * @param src       the elements of this object will be added/replaced to main object `obj`
+     * @returns {*}     object result
+     */
+    o.objMerge = function (objectBase, src) {
+        if (typeof objectBase !== 'object' || typeof src !== 'object')
+            return false;
+
+        if (Object.key) {
+            Object.keys(src).forEach(function (key) {
+                objectBase[key] = src[key];
+            });
+            return objectBase;
+        } else {
+            for (var key in src)
+                if (src.hasOwnProperty(key)) objectBase[key] = src[key];
+            return objectBase;
+        }
+    };
+
+    /**
+     * Merge objects if `objectBase` key not exists
+     * @param objectBase
+     * @param src
+     * @returns {*}
+     */
+    o.objMergeNotExists = function (objectBase, src) {
+        for (var key in src)
+            if (objectBase[key] === undefined)
+                objectBase[key] = src[key];
+        return objectBase;
+    };
+
+    /**
+     * Merge objects if `objectBase` key is exists
+     * @param objectBase
+     * @param src
+     * @returns {*}
+     */
+    o.objMergeOnlyExists = function (objectBase, src) {
+        for (var key in src)
+            if (objectBase[key] !== undefined)
+                objectBase[key] = src[key];
+        return objectBase;
+    };
+
+    /**
+     * Computes the difference of arrays
+     * Compares arr1 against one or more other arrays and returns the values in arr1
+     * that are not present in any of the other arrays.
+     * @param arr1
+     * @param arr2
+     * @returns {*}
+     */
+    o.arrDiff = function (arr1, arr2) {
+        if (o.isArr(arr1) && o.isArr(arr2)) {
+            return arr1.slice(0).filter(function (item) {
+                return arr2.indexOf(item) === -1;
+            })
+        }
+        return false;
+    };
+
+    /**
+     * Check on typeof is string a param
+     * @param param
+     * @returns {boolean}
+     */
+    o.isStr = function (param) {
+        return typeof param === 'string';
+    };
+
+    /**
+     * Check on typeof is array a param
+     * @param param
+     * @returns {boolean}
+     */
+    o.isArr = function (param) {
+        return Array.isArray(param);
+    };
+
+    /**
+     * Check on typeof is object a param
+     * @param param
+     * @returns {boolean}
+     */
+    o.isObj = function (param) {
+        return (param !== null && typeof param == 'object');
+    };
+
+    /**
+     * Determine param is a number or a numeric string
+     * @param param
+     * @returns {boolean}
+     */
+    o.isNum = function (param) {
+        return !isNaN(param);
+    };
+
+    /**
+     * Determine param to undefined type
+     * @param param
+     * @returns {boolean}
+     */
+    o.defined = function (param) {
+        return typeof(param) != 'undefined';
+    };
+
+    // Determine whether a variable is empty
+    o.isEmpty = function (param) {
+        return (param === "" || param === 0 || param === "0" || param === null || param === undefined || param === false || (o.isArr(param) && param.length === 0));
+    };
+
+    /**
+     * Javascript object to JSON data
+     * @param data
+     */
+    o.objToJson = function (data) {
+        return JSON.stringify(data);
+    };
+
+    /**
+     * JSON data to Javascript object
+     * @param data
+     */
+    o.jsonToObj = function (data) {
+        return JSON.parse(data);
+    };
+
+    /**
+     * Cleans the array of empty elements
+     * @param src
+     * @returns {Array}
+     */
+    o.cleanArr = function (src) {
+        var arr = [];
+        for (var i = 0; i < src.length; i++)
+            if (src[i]) arr.push(src[i]);
+        return arr;
+    };
+
+    /**
+     * Return type of data as name object "Array", "Object", "String", "Number", "Function"
+     * @param data
+     * @returns {string}
+     */
+    o.typeOf = function (data) {
+        return Object.prototype.toString.call(data).slice(8, -1);
+    };
+
+    /**
+     * Convert HTML form to encode URI string
+     * @param form
+     * @param asObject
+     * @returns {*}
+     */
+    o.formData = function (form, asObject) {
+        var obj = {}, str = '';
+        for (var i = 0; i < form.length; i++) {
+            var f = form[i];
+            if (f.type == 'submit' || f.type == 'button') continue;
+            if ((f.type == 'radio' || f.type == 'checkbox') && f.checked == false) continue;
+            var fName = f.nodeName.toLowerCase();
+            if (fName == 'input' || fName == 'select' || fName == 'textarea') {
+                obj[f.name] = f.value;
+                str += ((str == '') ? '' : '&') + f.name + '=' + encodeURIComponent(f.value);
+            }
+        }
+        return (asObject === true) ? obj : str;
+    };
+
+    /**
+     * HTML string convert to DOM Elements Object
+     * @param data
+     * @returns {*}
+     */
+    o.toNode = function (data) {
+        var parser = new DOMParser();
+        var node = parser.parseFromString(data, "text/xml");
+        console.log(node);
+        if (typeof node == 'object' && node.firstChild.nodeType == Node.ELEMENT_NODE)
+            return node.firstChild;
+        else return false;
+    };
+
+    /**
+     * Removes duplicate values from an array
+     * @param arr
+     * @returns {Array}
+     */
+    o.uniqueArr = function (arr) {
+        var tmp = [];
+        for (var i = 0; i < arr.length; i++) {
+            if (tmp.indexOf(arr[i]) == "-1") tmp.push(arr[i]);
+        }
+        return tmp;
+    };
+
+    /**
+     * Reads entire file into a string
+     * This function uses XmlHttpRequest and cannot retrieve resource from different domain.
+     * @param url
+     * @returns {*|string|null|string}
+     */
+    o.fileGetContents = function (url) {
+        var req = null;
+        try {
+            req = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                req = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                try {
+                    req = new XMLHttpRequest();
+                } catch (e) {
+                }
+            }
+        }
+        if (req == null) throw new Error('XMLHttpRequest not supported');
+        req.open("GET", url, false);
+        req.send(null);
+        return req.responseText;
+    };
+
+    /**
+     * Calculates the position and size of elements.
+     *
+     * @param elem
+     * @returns {{y: number, x: number, width: number, height: number}}
+     */
+    o.getPosition = function (elem) {
+        var top = 0, left = 0;
+        if (elem.getBoundingClientRect) {
+            var box = elem.getBoundingClientRect();
+            var body = document.body;
+            var docElem = document.documentElement;
+            var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+            var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+            var clientTop = docElem.clientTop || body.clientTop || 0;
+            var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+            top = box.top + scrollTop - clientTop;
+            left = box.left + scrollLeft - clientLeft;
+            return {y: Math.round(top), x: Math.round(left), width: elem.offsetWidth, height: elem.offsetHeight};
+        } else { //fallback to naive approach
+            while (elem) {
+                top = top + parseInt(elem.offsetTop, 10);
+                left = left + parseInt(elem.offsetLeft, 10);
+                elem = elem.offsetParent;
+            }
+            return {y: top, x: left, width: elem.offsetWidth, height: elem.offsetHeight};
+        }
+    };
+
+    /**
+     * Returns the coordinates of the mouse on any element
+     * @param element
+     * @param event
+     * @returns {{x: number, y: number}}
+     */
+    o.getMouseElement = function (element, event) {
+        if(element instanceof HTMLElement && event instanceof MouseEvent) {
+            var x = event.pageX - element.offsetLeft;
+            var y = event.pageY - element.offsetTop;
+            return {x: x, y: y};
+        }else
+            return false;
+    };
+
+    /**
+     * Returns the coordinates of the mouse on the canvas element
+     * @param canvas
+     * @param event
+     * @returns {{x: number, y: number}}
+     */
+    o.getMouseCanvas = function (canvas, event) {
+        if((canvas instanceof HTMLCanvasElement || canvas.getBoundingClientRect) && event instanceof MouseEvent){
+            var rect = canvas.getBoundingClientRect();
+            return {
+                x: event.clientX - rect.left,
+                y: event.clientY - rect.top
+            };
+        }else
+            return false;
+    };
+
+    /**
+     * Creator of styles, return style-element or style-text.
+     *
+     * <pre>var style = createStyle('body','font-size:10px');
+     *style.add('body','font-size:10px')       // added style
+     *style.add( {'background-color':'red'} )  // added style
+     *style.getString()                        // style-text
+     *style.getObject()                        // style-element</pre>
+     *
+     * @param selector      name of selector styles
+     * @param property      string "display:object" or object {'background-color':'red'}
+     * @returns {*}         return object with methods : getString(), getObject(), add()
+     */
+    o.createStyle = function (selector, property) {
+        var o = {
+            content: '',
+            getString: function () {
+                return '<style rel="stylesheet">' + "\n" + o.content + "\n" + '</style>';
+            },
+            getObject: function () {
+                var st = document.createElement('style');
+                st.setAttribute('rel', 'stylesheet');
+                st.textContent = o.content;
+                return st;
+            },
+            add: function (select, prop) {
+                if (typeof prop === 'string') {
+                    o.content += select + "{" + ( (prop.substr(-1) == ';') ? prop : prop + ';' ) + "}";
+                } else if (typeof prop === 'object') {
+                    o.content += select + "{";
+                    for (var key in prop)
+                        o.content += key + ':' + prop[key] + ';';
+                    o.content += "}";
+                }
+                return this;
+            }
+        };
+        return o.add(selector, property);
+    };
+
+    /**
+     * Create new NodeElement
+     * @param tag       element tag name 'p, div, h3 ... other'
+     * @param attrs     object with attributes key=value
+     * @param inner     text, html or NodeElement
+     * @returns {Element}
+     */
+    o.createNode = function (tag, attrs, inner) {
+        var elem = document.createElement(tag);
+        if (typeof attrs === 'object') {
+            for (var key in prop)
+                elem.setAttribute(key, prop[key]);
+        }
+
+        if (typeof inner === 'string') {
+            elem.innerHTML = inner;
+        } else if (typeof inner === 'object') {
+            elem.appendChild(elem);
+        }
+        return elem;
+    };
+
+
+    /**
+     * Returns a random integer between min, max, if not specified the default of 0 to 100
+     * @param min
+     * @param max
+     * @returns {number}
+     */
+    o.rand = function (min, max) {
+        min = min || 0;
+        max = max || 100;
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    /**
+     * Returns random string color, HEX format
+     * @returns {string}
+     */
+    o.randColor = function () {
+        var letters = '0123456789ABCDEF'.split(''),
+            color = '#';
+        for (var i = 0; i < 6; i++)
+            color += letters[Math.floor(Math.random() * 16)];
+        return color;
+    };
+
+    /**
+     * Converts degrees to radians
+     * @param deg
+     * @returns {number}
+     */
+    o.degreesToRadians = function (deg) {
+        return (deg * Math.PI) / 180;
+    };
+
+    /**
+     * Converts radians to degrees
+     * @param rad
+     * @returns {number}
+     */
+    o.radiansToDegrees = function (rad) {
+        return (rad * 180) / Math.PI;
+    };
+
+    /**
+     * The calculation of the distance between points
+     * The point is an object with properties `x` and `y` {x:100,y:100}
+     * @param point1
+     * @param point2
+     * @returns {number}
+     */
+    o.distanceBetween = function (point1, point2) {
+        var dx = point2.x - point1.x;
+        var dy = point2.y - point1.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    };
+
+
+    window.Util = o;
+
+})(window);
